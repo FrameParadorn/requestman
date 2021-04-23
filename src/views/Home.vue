b863dc5450cadec2aef48a6fa5462f49200099f1
Paradorn Chandeth <paradorn@qboxpoint.com>
2021-04-23 11:20:06 +0700

    Resize layout address bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,11 @@
     <v-col class="d-flex" cols="2">
       <v-select v-model="itemSelect" :items="items" item-value="GET" solo></v-select>
     </v-col>
-    <v-col class="d-flex" cols="8">
-      <v-text-field label="http://example.com/params=1&params=2" solo></v-text-field>
+    <v-col class="d-flex" cols="9">
+      <v-text-field label="http://example.com" solo></v-text-field>
     </v-col>
-    <v-col class="d-flex" cols="2">
-      <div class="text-xs-center">
-        <v-btn elevation="2" large> Send </v-btn>
-      </div>
+    <v-col class="d-flex" cols="1">
+        <v-btn elevation="2" large block> Send </v-btn>
     </v-col>
   </v-row>
 </template>
